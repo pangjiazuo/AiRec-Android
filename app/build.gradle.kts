e922e2502b9f508dc1baad199e92777cc54a9e1c
pangjiazuo <pangjiazuo@inmolens.com>
2026-09-14 14:12:19 +0800

    chore: 统一安卓客户端版本为 1.0.0
    Change-Id: Ibdf25164287b0779c8f0f18a1293c530f2018a80

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.neardi.recorder"
         minSdk = 31
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.5.1"
+        versionCode = 8
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
